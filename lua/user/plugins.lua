4e2f7e2f86439e0aab96de63b62e63d819f261f9
Arash Jahdkaran <77935130+jahdkaran@users.noreply.github.com>
2022-08-11 10:34:30 +0430

    Add lsp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,7 +51,6 @@ return packer.startup(function(use)
   use "rebelot/kanagawa.nvim"
   use 'navarasu/onedark.nvim'
   use 'Mofiqul/vscode.nvim'
-  use "eiginn/netrw" 
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
@@ -59,11 +58,16 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
+    -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
